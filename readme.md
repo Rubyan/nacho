464fcd7786776bfa6afe9cfdeb7ca1bbbedc5bea
Johan van der Kuijl <johan.vanderkuijl@kpn.com>
2016-02-16 06:42:18 +0100

    first release

---
+++ readme.md
@@ -0,0 +1,2 @@
+<b>Nacho testing code</b>
+http://culttt.com/2014/05/07/create-psr-4-php-package/
---
+++ src/Nacho.php
@@ -0,0 +1,10 @@
+<?php namespace Rubyan\Nacho;
+ 
+class Nacho {
+ 
+  public function hasCheese($bool = true)
+  {
+    return $bool;
+  }
+ 
+}
